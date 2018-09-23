d64d7f763a63f075880d788177123a185cd314ff
unknown <obhutara@gmail.com>
2018-09-23 23:20:53 +0200

    debug code in theme

--- R/theme.R
+++ R/theme.R
@@ -8,9 +8,9 @@
 theme_linko<-function(){
   ggplot2::theme_bw()+
     ggplot2::theme(panel.grid.major = element_line(color="#54d8e0",linetype = 1),
-                   panel.grid.minor = element_rect(color="#54d8e0",linetype = 5)
+                   panel.grid.minor = element_rect(color="#54d8e0",linetype = 5),
                    panel.background = element_rect(fill ="#54d8e0"),
-                   plot.background = gelement_rect(),
+                   plot.background = element_rect(),
                    panel.border = ggplot2::element_blank(),
                    text=ggplot2::element_text(color="#54d8e0"),
                    axis.text = ggplot2::element_text(color="#54d8e0"),
