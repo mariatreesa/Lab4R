f007cd68dcb16627401965513f408243ae6448a8
Maria Treesa Sebastian <mariatreesaa@gmail.com>
2018-09-23 23:27:10 +0200

    commenting theme to check build

--- R/theme.R
+++ R/theme.R
@@ -1,22 +1,22 @@
-#'LIU Theme
-#'@importFrom ggplot2 theme theme_bw element_rect element_line element_blank element_text margin
-#'@importFRom grid unit
-#'
-#'return a theme for ggplot2 outputs
-#'@export
+#LIU Theme
+#@importFrom ggplot2 theme theme_bw element_rect element_line element_blank element_text margin
+#@importFRom grid unit
+#
+#return a theme for ggplot2 outputs
+#@export
 
-theme_linko<-function(){
-  ggplot2::theme_bw()+
-    ggplot2::theme(panel.grid.major = element_line(color="#54d8e0",linetype = 1),
-                   panel.grid.minor = element_rect(color="#54d8e0",linetype = 5),
-                   panel.background = element_rect(fill ="#54d8e0"),
-                   plot.background = element_rect(),
-                   panel.border = ggplot2::element_blank(),
-                   text=ggplot2::element_text(color="#54d8e0"),
-                   axis.text = ggplot2::element_text(color="#54d8e0"),
-                   axis.ticks = ggplot2::element_line(color="#54d8e0"),
-                   axis.line.x.bottom = element_line(color="#54d8e0",grid::arrow=(length =grid::unit(0.1,"cm")),
-                    axis.line.y.left = element_line(color="#54d8e0")
-                  )
-  
-}
+#theme_linko<-function(){
+ # ggplot2::theme_bw()+
+ #   ggplot2::theme(panel.grid.major = element_line(color="#54d8e0",linetype = 1),
+ #                  panel.grid.minor = element_rect(color="#54d8e0",linetype = 5),
+ #                  panel.background = element_rect(fill ="#54d8e0"),
+ #                  plot.background = element_rect(),
+ #                  panel.border = ggplot2::element_blank(),
+ #                  text=ggplot2::element_text(color="#54d8e0"),
+ #                  axis.text = ggplot2::element_text(color="#54d8e0"),
+ #                  axis.ticks = ggplot2::element_line(color="#54d8e0"),
+ #                  axis.line.x.bottom = element_line(color="#54d8e0",grid::arrow=(length =grid::unit(0.1,"cm")),
+ #                   axis.line.y.left = element_line(color="#54d8e0")
+ #                 )
+
+#}
